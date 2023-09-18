b6941e9b9a021aeda715b6b8cef06b8db6fd322b
Adriana Salas <adriana.salas@mrjaffesclass.com>
2023-09-18 11:47:07 -0700

    commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,5 +9,13 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringEqual = string1.equals(string3); //for string1 and 3
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(stringEqual);
     }
 }
